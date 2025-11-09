df877a642b03b5288ef1b333166fdb9646744edc
marlon307 <marlon-ramosb@hotmail.com>
2025-11-09 00:51:26 -0300

    cfg

--- client.cpp
+++ client.cpp
@@ -8,56 +8,65 @@
 
 #define PORT 54000
 
-void receive_messages(int sock) {
-    char buffer[4096];
-    while (true) {
-        memset(buffer, 0, 4096);
-        int bytes_recv = recv(sock, buffer, 4096, 0);
-        if (bytes_recv <= 0) {
-            std::cout << "Desconectado do servidor.\n";
-            break;
-        }
-        std::cout << "\r" << buffer << "\nVocê: " << std::flush;
-    }
+void receive_messages(int sock)
+{
+	char buffer[4096];
+	while (true)
+	{
+		memset(buffer, 0, 4096);
+		int bytes_recv = recv(sock, buffer, 4096, 0);
+		if (bytes_recv <= 0)
+		{
+			std::cout << "Desconectado do servidor.\n";
+			break;
+		}
+		std::cout << "\r" << buffer << "\nVocê: " << std::flush;
+	}
 }
 
-int main() {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-        std::cerr << "Erro ao criar socket\n";
-        return 1;
-    }
+int main()
+{
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1)
+	{
+		std::cerr << "Erro ao criar socket\n";
+		return 1;
+	}
 
-    sockaddr_in server_addr{};
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+	sockaddr_in server_addr{};
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	inet_pton(AF_INET, "66.241.125.150", &server_addr.sin_addr);
 
-    if (connect(sock, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        std::cerr << "Erro ao conectar ao servidor\n";
-        return 1;
-    }
+	if (connect(sock, (sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+	{
+		std::cerr << "Erro ao conectar ao servidor\n";
+		return 1;
+	}
 
-    std::cout << "Conectado ao chat!\n";
+	std::cout << "Conectado ao chat!\n";
 
-    std::thread(receive_messages, sock).detach();
+	std::thread(receive_messages, sock).detach();
 
-    while (true) {
-        std::string msg;
-        std::cout << "Você: ";
-        std::getline(std::cin, msg);
+	while (true)
+	{
+		std::string msg;
+		std::cout << "Você: ";
+		std::getline(std::cin, msg);
 
-        if (msg == "/sair") {
-            break;
-        }
+		if (msg == "/sair")
+		{
+			break;
+		}
 
-        int send_res = send(sock, msg.c_str(), msg.size() + 1, 0);
-        if (send_res == -1) {
-            std::cerr << "Erro ao enviar mensagem\n";
-            break;
-        }
-    }
+		int send_res = send(sock, msg.c_str(), msg.size() + 1, 0);
+		if (send_res == -1)
+		{
+			std::cerr << "Erro ao enviar mensagem\n";
+			break;
+		}
+	}
 
-    close(sock);
-    return 0;
+	close(sock);
+	return 0;
 }
